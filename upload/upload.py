fc1017d0be5736b8808499de6016119fd70492ef
Gilbert1Rosa <gilbertli2033@gmail.com>
2023-09-04 23:21:17 -0400

    Adding firefox profile to use google account,  because google is also a bitch.

--- upload/upload.py
+++ upload/upload.py
@@ -1,8 +1,14 @@
+import copy
 from time import sleep
 
 from selenium import webdriver
+from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.options import FirefoxProfile
+
+await_time_in_seconds = 2
 
 
 def test_selenium():
@@ -21,24 +27,30 @@ def login_to_tiktok():
     """
         Scrapping to login to tiktok.
     """
-    driver = webdriver.Firefox()
+    # Create custom firefox profile for selenium
+    profile_path = "C:\\Users\\Gilbert\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\qp71cu4y.Selenium"
+    options = FirefoxOptions()
+    options.profile = FirefoxProfile(profile_path)
+    driver = Firefox(options)
     driver.get("https://www.tiktok.com/")
     login_button = driver.find_element(By.ID, "header-login-button")
     login_button.click()
-    sleep(2)
+    sleep(await_time_in_seconds)
     login_google_button = None
     elements = driver.find_elements(By.CLASS_NAME, "tiktok-1j4ihbo-DivBoxContainer")
     for element in elements:
-        if element.text == "Continuar con Google":
+        if element.text == "Continue with Google":
             login_google_button = element
             break
     login_google_button.click()
-    sleep(200)
+    sleep(await_time_in_seconds)
+    handles = copy.copy(driver.window_handles)
+    main_window = driver.current_window_handle
+    handles.remove(main_window)
+    driver.switch_to.window(handles[0])
+    account_button = driver.find_element(By.CLASS_NAME, 'd2laFc')
+    account_button.click()
     driver.close()
-    """
-        When emergent window, asking for google login opens up,
-        put login credentials.
-    """
 
 
 def upload_video():
